0788e55d05e1f3f67d9d09883281dc2ccd987cdd
vivekemmadi <115087955+vivekemmadi@users.noreply.github.com>
2025-06-11 22:07:12 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
